68ceef5f1209182ea690f26f5613dec73e444e0e
GabrielAssencao <gabriel.souza59.jesus@gmail.com>
2026-05-20 14:50:49 -0300

    Codigo de db com novas regras

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,7 @@ console.log("Configurando conexão com host:", process.env.DB_HOST);
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306, // Garante que use a porta correta
   ssl: {
